60b325db50d58ccd59eaaf6453af93747a1ce0dc
Aditya Aupta <guptaaditya854@gmail.com>
2022-06-24 18:50:40 +0530

    .

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details. This
+        is an introductory level project to understand the concepts of React and working with globally available API's.
+      
+        .
+      </p>
+     
+    </>
+  )
+}
+
+export default About
